74b1f1e41376a8c72f465a8e3166050d031ae3c7
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-09-25 10:58:14 -0600

    T0.1.2 Feat: se configuro el JavaFX

--- src/org/kinalllaves/system/Main.java
+++ src/org/kinalllaves/system/Main.java
@@ -1,20 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.kinalllaves.system;
 
-/**
- *
- * @author informatica
- */
-public class Main {
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Main extends Application {
+
+    private static Stage escenarioPrincipal;
+
+    @Override
+    public void start(Stage stage) {
+        escenarioPrincipal = stage;
+        escenarioPrincipal.setResizable(true);
+
+     
+        cambiarEscena("/org/kinalllaves/view/login.fxml", "KinalLlaves | Iniciar sesión");
     }
+
     
+    public static void cambiarEscena(String rutaFXML, String titulo) {
+        try {
+            URL url = Main.class.getResource(rutaFXML);
+            if (url == null) {
+                throw new IOException("No se encontró el recurso FXML: " + rutaFXML);
+            }
+
+            Parent raiz = FXMLLoader.load(url);
+            Scene escena = new Scene(raiz);
+
+            URL css = Main.class.getResource("/org/kinalllaves/view/style/styles.css");
+            if (css != null) {
+                escena.getStylesheets().add(css.toExternalForm());
+            }
+
+            escenarioPrincipal.setTitle(titulo);
+            escenarioPrincipal.setScene(escena);
+            escenarioPrincipal.sizeToScene();
+            escenarioPrincipal.centerOnScreen();
+            escenarioPrincipal.show();
+
+        } catch (Exception e) {
+            
+            System.out.println("Aviso: No se pudo cargar '" + rutaFXML + "'. Desplegando pantalla en blanco de prueba.");
+
+            StackPane raizBlanca = new StackPane();
+            Scene escenaBlanca = new Scene(raizBlanca, 760, 560);
+
+            escenarioPrincipal.setTitle(titulo);
+            escenarioPrincipal.setScene(escenaBlanca);
+            escenarioPrincipal.centerOnScreen();
+            escenarioPrincipal.show();
+        }
+    }
+
+    public static Stage getEscenarioPrincipal() {
+        return escenarioPrincipal;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
